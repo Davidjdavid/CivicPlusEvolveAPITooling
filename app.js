a86b870f977b6d71ae2339692f39d19562ce6b96
davidjdavd <dave.j.hazelwood@gmail.com>
2026-08-20 10:16:03 -0500

    Setup of app.js and index.html to make sure CPanel is displaying node.js correctly

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('Error loading index.html');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
+});
+
+server.listen(port);
